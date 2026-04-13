8931ad892113dd8685b6279050cefcf22ba0459b
RominaSchoeni <romina.schoeni@rimon-ar.ch>
2026-04-13 23:10:41 +0200

    Add Hackathon Hack Zurich project

--- assets/manifest.js
+++ assets/manifest.js
@@ -5,5 +5,6 @@
   "exchange": ["01.%20ahero.jpg", "01-hero.png", "02-screen.png", "03-screen.png", "04-screen.png"],
   "animation": ["animation.png"],
   "brand": ["01.png", "02.gif", "03.png", "04.png", "06.png", "07.jpg", "08.jpg"],
-  "cube": ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "20200414_123112.jpg"]
+  "cube": ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "20200414_123112.jpg"],
+  "hackathon": []
 };
